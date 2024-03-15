faabe3410645dd540666870a80958021e2659748
Struckmeier Eike (ETAS-VCS/XPC-Be2) <ste6be@bosch.com>
2024-03-15 15:46:14 +0100

    links for reaserach

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # MachineLearningFoundations
+
+Build LLM from scratch
+ https://www.youtube.com/watch?v=UU1WVnMk4E8
+
+ Linear Regression from scratch
+ https://www.youtube.com/watch?v=VmbA0pi2cRQ
+
+ Pricing OpenAI
+ https://openai.com/pricing
+
+ Langchain4j
+ https://www.youtube.com/watch?v=LIfpKwCig14
+
+ springboot API Implementation
+ https://www.youtube.com/watch?v=N5HqaIBT1v0
--- linearRegression.py
+++ linearRegression.py
@@ -5,3 +5,14 @@ data = pd.read_csv('data.csv')
 
 plt.scatter(data.experience, data.wage)
 plt.show()
+
+def loss_function(m, b, points):
+    total_error = 0
+    for i = in range(len(points)):
+        x = points.iloc[i].experience
+        y = points.iloc[i].wage
+        total_error += (y - (m * x + b)) ** 2
+   total_error / float(len(points))     
+
+def gradient_descent(m_now, b_now, points, L):
+    m_gradient = 0
